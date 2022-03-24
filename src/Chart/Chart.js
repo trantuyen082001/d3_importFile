59b99a0f7c8506db0b5533dff3903e95a6835e53
trantuyen082001 <trantuyen082001@gmail.com>
2022-03-23 23:54:18 -1200

    update super code

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -1,14 +1,14 @@
-import React, {useRef, useMemo, useEffect} from 'react';
+import React, { useRef, useMemo, useEffect } from 'react';
 import * as d3 from 'd3';
 
-const Chart = ({data = [], dimensions = {}}) => {
+const Chart = ({ data = [], dimensions = {} }) => {
 
   const svgRef = React.useRef(null);
   const { width, height, margin = {} } = dimensions;
   const svgWidth = width + margin.left + margin.right;
   const svgHeight = height + margin.top + margin.bottom;
 
-  
+
   useEffect(() => {
 
     const xScale = d3
@@ -19,70 +19,70 @@ const Chart = ({data = [], dimensions = {}}) => {
     const yScale = d3
       .scaleLinear()
       .domain([
-        d3.min(data[0].items, (d) => d.value) -5,
-        d3.max(data[0].items, (d) => d.value) +5
+        d3.min(data[0].items, (d) => d.value) - 5,
+        d3.max(data[0].items, (d) => d.value) + 5
       ])
       .range([height, 0]);
 
-      const svgEl = d3.select(svgRef.current);
-      svgEl.selectAll("*").remove(); // Clear svg content before adding new elements
-      const svg = svgEl
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+    const svgEl = d3.select(svgRef.current);
+    svgEl.selectAll("*").remove(); // Clear svg content before adding new elements
+    const svg = svgEl
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
 
 
-      // Add X grid lines with labels
-     const xAxis = d3
-        .axisBottom(xScale)
-        .ticks(14)
-        .tickSize(-height + margin.bottom);
+    // Add X grid lines with labels
+    const xAxis = d3
+      .axisBottom(xScale)
+      .ticks(14)
+      .tickSize(-height + margin.bottom);
 
-     const xAxisGroup = svg
-        .append("g")
-        .attr("transform", `translate(0, ${height - margin.bottom})`)
-        .call(xAxis);
+    const xAxisGroup = svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(xAxis);
 
-      xAxisGroup.select(".domain").remove();
-      xAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
-      xAxisGroup
-          .selectAll("text")
-          .attr("opacity", 0.5)
-          .attr("color", "white")
-          .attr("font-size", "0.75rem");
+    xAxisGroup.select(".domain").remove();
+    xAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
+    xAxisGroup
+      .selectAll("text")
+      .attr("opacity", 0.5)
+      .attr("color", "white")
+      .attr("font-size", "0.75rem");
 
-      const yAxis = d3
-          .axisLeft(yScale)
-          .ticks(10)
-          .tickSize(-width)
-          .tickFormat((val) => `${val}`);
+    const yAxis = d3
+      .axisLeft(yScale)
+      .ticks(10)
+      .tickSize(-width)
+      .tickFormat((val) => `${val}`);
 
-      const yAxisGroup = svg.append("g").call(yAxis);
-          yAxisGroup.select(".domain").remove();
-          yAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
-          yAxisGroup
-            .selectAll("text")
-            .attr("opacity", 0.5)
-            .attr("color", "white")
-            .attr("font-size", "0.75rem");
+    const yAxisGroup = svg.append("g").call(yAxis);
+    yAxisGroup.select(".domain").remove();
+    yAxisGroup.selectAll("line").attr("stroke", "rgba(255, 255, 255, 0.2)");
+    yAxisGroup
+      .selectAll("text")
+      .attr("opacity", 0.5)
+      .attr("color", "white")
+      .attr("font-size", "0.75rem");
 
-      // Draw the lines
+    // Draw the lines
     const line = d3
-        .line()
-        .x((d) => xScale(d.date))
-        .y((d) => yScale(d.date));
-      
-        svg
-        .selectAll(".line")
-        .data(data)
-        .enter()
-        .append("path")
-        .attr("fill", "none")
-        .attr("stroke", (d) => d.color)
-        .attr("stroke-width", 3)
-        .attr("d", (d) => line(d.items));
+      .line()
+      .x((d) => xScale(d.date))
+      .y((d) => yScale(d.value));
+
+    svg
+      .selectAll(".line")
+      .data(data)
+      .enter()
+      .append("path")
+      .attr("fill", "none")
+      .attr("stroke", (d) => d.color)
+      .attr("stroke-width", 3)
+      .attr("d", (d) => line(d.items));
   }, [data])
 
-  
+
 
   return (
     <svg ref={svgRef} width={svgWidth} height={svgHeight} />
